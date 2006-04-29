ea1e44d43840ad30ba239c95de2fc3e24fa72e1b
Jun Futagawa <jfut@integ.jp>
2006-04-29 16:50:09 +0000

    Revision 28 のコミット漏れ追加
    git-svn-id: https://www.seasar.org/svn/sandbox/s2directory/trunk@29 d4ad07e3-2afd-0310-aea9-bd47cc606c58

---
+++ s2directory/s2-directory-examples/src/main/java/examples/directorydao/directorydao/PosixGroupDao.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.directorydao.directorydao;
+
+import java.util.List;
+import examples.directorydao.entity.PosixGroup;
+
+/**
+ * PosixAccountのDaoインタフェースです。
+ * 
+ * @author Jun Futagawa (Integsystem Corporation)
+ * @version $Date::                           $
+ */
+public interface PosixGroupDao {
+	public Class BEAN = PosixGroup.class;
+	public static final String getPosixGroupList_QUERY = "objectclass=posixGroup";
+
+	public List getPosixGroupList();
+
+	public PosixGroup getPosixGroup(PosixGroup group);
+
+	public List getPosixGroupList(PosixGroup group);
+	public static final String getPosixGroupByCn_ARGS = "cn";
+
+	public PosixGroup getPosixGroupByCn(String cn);
+	public static final String getPosixGroupListByMemberUid_ARGS = "memberUid";
+
+	public List getPosixGroupListByMemberUid(String memberUid);
+
+	public static final String getPosixGroupListByMemberUid2_ARGS = "memberUid";
+	public List getPosixGroupListByMemberUid2(List memberUid);
+
+	public int insert(PosixGroup group);
+
+	public int update(PosixGroup group);
+
+	public int delete(PosixGroup group);
+
+	public List getAllGroup();
+}
---
+++ s2directory/s2-directory-examples/src/main/java/examples/directorydao/directorydao/PosixGroupWithListDao.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.directorydao.directorydao;
+
+import java.util.List;
+import examples.directorydao.entity.PosixGroupWithList;
+
+/**
+ * PosixAccountのDaoインタフェースです。
+ * 
+ * @author Jun Futagawa (Integsystem Corporation)
+ * @version $Date::                           $
+ */
+public interface PosixGroupWithListDao {
+	public Class BEAN = PosixGroupWithList.class;
+	public String OBJECTCLASSES = "posixGroup";
+	
+	public static final String getPosixGroupList_QUERY = "objectclass=posixGroup";
+
+	public List getPosixGroupList();
+
+	public PosixGroupWithList getPosixGroup(PosixGroupWithList group);
+
+	public List getPosixGroupList(PosixGroupWithList group);
+	public static final String getPosixGroupByCn_ARGS = "cn";
+
+	public PosixGroupWithList getPosixGroupByCn(String cn);
+	public static final String getPosixGroupListByMemberUid_ARGS = "memberUid";
+
+	public List getPosixGroupListByMemberUid(String memberUid);
+
+	public static final String getPosixGroupListByMemberUid2_ARGS = "memberUid";
+	public List getPosixGroupListByMemberUid2(List memberUid);
+
+	public int insert(PosixGroupWithList group);
+
+	public int update(PosixGroupWithList group);
+
+	public int delete(PosixGroupWithList group);
+
+	public List getAllGroup();
+}
