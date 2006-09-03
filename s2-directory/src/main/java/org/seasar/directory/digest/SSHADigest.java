25fd4152873c92da7f5c1fb2577f97f1dd77195a
Jun Futagawa <jfut@integ.jp>
2006-09-03 11:51:01 +0000

    S2ContainerのBase64Utilを使うことで、依存関係からcommons-codecを無くしました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2directory/trunk/s2directory@58 d4ad07e3-2afd-0310-aea9-bd47cc606c58

--- s2-directory/src/main/java/org/seasar/directory/digest/SSHADigest.java
+++ s2-directory/src/main/java/org/seasar/directory/digest/SSHADigest.java
@@ -31,4 +31,11 @@ public class SSHADigest extends SHADigest {
 	public String create(String password) {
 		return LABEL + super.create(DigestUtils.getRandomSalt(), password);
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean verify(String digest, String password) {
+		return super.verify(digest.substring(LABEL.length()), password, 20);
+	}
 }
